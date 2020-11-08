bde9e5b964e3bcac7cb69f195f6f312c1e88242b
ShepelVitalii <63495011+ShepelVitalii@users.noreply.github.com>
2020-11-08 17:04:23 +0200

    pnotify message for wrong message and end of list

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,56 +1,62 @@
-import { error } from "@pnotify/core";
+import { error, info } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
-const API_KEY = '18992222-0ffbc097a98d577b6731791a7'
-const BASE_URL = 'https://pixabay.com/api'
+const API_KEY = '18992222-0ffbc097a98d577b6731791a7';
+const BASE_URL = 'https://pixabay.com/api';
 
-export default class ApiService{
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    try {
+      const response = await fetch(
+        `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+      );
+
+      const newHits = await response.json();
+      this.incrementPage();
+
+      return newHits.hits;
+    } catch (info) {
+      this.noMorePicturesLeft();
     }
+  }
 
-    async fetchImages() {
-        
-        try {
-            const response = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
-        
-            const newHits = await response.json();
-            this.incrementPage();
-            
+  incrementPage() {
+    this.page += 1;
+  }
 
-            return newHits.hits;
-        } catch (error) {
-            this.errorMessage();
-        }
-        
-    }
-    
+  resetPage() {
+    this.page = 1;
+  }
 
-    incrementPage() {
-        this.page += 1;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
-    resetPage() {
-        this.page = 1;
-    }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 
-    get query() {
-        return this.searchQuery;
-    }
-
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
-
-    errorMessage(){
+  errorMessage() {
     error({
-        title: 'Что-то пошло не так',
+      title: 'Что-то пошло не так',
       text: 'Но ничего страшного, мы над этим работаем',
       delay: 5000,
-    closerHover: true,
-    })
-}
-    
+      closerHover: true,
+    });
+  }
+
+  noMorePicturesLeft() {
+    info({
+      title: 'Опа, картинки закончились',
+      text: 'Попробуйте поискать что нибудь другое',
+      delay: 5000,
+      closerHover: true,
+    });
+  }
 }
--- src/js/img-finder.js
+++ src/js/img-finder.js
@@ -5,92 +5,102 @@ import ApiImages from './apiService';
 import LoadMoreBtn from './load-more-btn';
 import animateScrollTo from 'animated-scroll-to';
 import { onOpenModal } from './modal';
-import { info } from "@pnotify/core";
+import { info } from '@pnotify/core';
 
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    imgCardContainer: document.querySelector('.js-card-container'),
-    imgContainer: document.querySelector('.gallery')
-}
+  searchForm: document.querySelector('.js-search-form'),
+  imgCardContainer: document.querySelector('.js-card-container'),
+  imgContainer: document.querySelector('.gallery'),
+  //   btnToHide: document.querySelector('.is-hidden'),
+};
 const loadMoreBtn = new LoadMoreBtn({
-    selector: '[data-action="load-more"]',
-    hidden: true,
+  selector: '[data-action="load-more"]',
+  hidden: true,
 });
 
-const apiImages = new ApiImages(); 
+const apiImages = new ApiImages();
 
 refs.searchForm.addEventListener('submit', onSearch);
 
-loadMoreBtn.refs.button.addEventListener('click', fetchHits)
-refs.imgContainer.addEventListener('click', onOpenModal)
+loadMoreBtn.refs.button.addEventListener('click', fetchHits);
+refs.imgContainer.addEventListener('click', onOpenModal);
 
+async function onSearch(e) {
+  e.preventDefault();
+  clearImgContainer();
+  apiImages.query = e.currentTarget.elements.query.value;
 
-async function onSearch (e) {
-    
-    e.preventDefault();
-    clearImgContainer();
-    apiImages.query = e.currentTarget.elements.query.value;
+  try {
+    loadMoreBtn.show();
+    loadMoreBtn.disable();
+    apiImages.resetPage();
+    fetchHits();
 
-    try {
-        
-        loadMoreBtn.show();
-        loadMoreBtn.disable();
-        apiImages.resetPage();
-        fetchHits();
-        
-        if (apiImages.query === '') {
-            return emptyStringMessage()
-        }
-    } catch (error) {
-        apiImages.errorMessage();
+    if (apiImages.query === '') {
+      return emptyStringMessage();
     }
-};
+  } catch (error) {
+    apiImages.errorMessage();
+  }
+}
 
 function animateScroll() {
-    const indexToScroll = 12 * (apiImages.page - 1)- 11;
-    const itemToScroll = refs.imgContainer.children[indexToScroll];
-    const options = {
+  const indexToScroll = 12 * (apiImages.page - 1) - 11;
+  const itemToScroll = refs.imgContainer.children[indexToScroll];
+  const options = {
     speed: 500,
     verticalOffset: -10,
-    };
+  };
 
-    animateScrollTo(itemToScroll, options)
+  animateScrollTo(itemToScroll, options);
 }
 
 async function fetchHits() {
-    loadMoreBtn.disable();
+  loadMoreBtn.disable();
 
-    try {
-        const response = await apiImages.fetchImages()
-        imagesMurkup(response);
-    
-        loadMoreBtn.enable();
-        
-        
-    } catch (error) {
-        apiImages.errorMessage();
+  try {
+    const response = await apiImages.fetchImages();
+    if (response.length < 11) {
+      loadMoreBtn.hide();
     }
-    animateScroll()
+    if (response.length === 0) {
+      noPicturesAtAll();
+    }
+
+    imagesMurkup(response);
+
+    loadMoreBtn.enable();
+  } catch (error) {
+    loadMoreBtn.hide();
+  }
+  animateScroll();
 }
 
 function imagesMurkup(hits) {
-    refs.imgCardContainer.insertAdjacentHTML('beforeend', imgCardTpl(hits));
-    
+  refs.imgCardContainer.insertAdjacentHTML('beforeend', imgCardTpl(hits));
 }
 
 function clearImgContainer() {
-    refs.imgCardContainer.innerHTML = '';
-    
+  refs.imgCardContainer.innerHTML = '';
 }
 
-function emptyStringMessage(){
-    info({
-        title: 'Нет информации по поиску тегов',
-      text: 'Но ничего страшного, мы вам что-нибудь подберерем',
-      delay: 5000,
+function emptyStringMessage() {
+  info({
+    title: 'Нет информации по поиску тегов',
+    text: 'Но ничего страшного, мы вам что-нибудь подберерем',
+    delay: 5000,
     closerHover: true,
-    })
+  });
+}
+
+function noPicturesAtAll() {
+  info({
+    title: 'Такого у нас нет',
+    text: 'Давайте попробуем что нибудь другое',
+    delay: 5000,
+    closerHover: true,
+  });
 }
